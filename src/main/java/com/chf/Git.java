157a5b5e3cf0cc7f990e3f917871feef5c02afaa
hong <hong.@qq.com>
2022-12-21 23:23:55 +0800

    first branch first test

--- src/main/java/com/chf/Git.java
+++ src/main/java/com/chf/Git.java
@@ -4,5 +4,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("用于测试的Java代码");
         System.out.println("进行了第二次修改");
+
+        System.out.println("在分支上进行修改的并且提交代码");
     }
 }

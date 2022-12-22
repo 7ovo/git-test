c390abab522ddee9b8477ba8e1042668cd89da01
7ovo <121172964+7ovo@users.noreply.github.com>
2022-12-22 12:53:55 +0800

    GitHub test

--- src/main/java/com/chf/Git.java
+++ src/main/java/com/chf/Git.java
@@ -12,5 +12,7 @@ public class Git {
         System.out.println("在合并分支后的分支上修改了代码并提交");
 
         System.out.println("分享到了本地仓库而后进行了修改且提交");
+        
+        System.out.println("我在GitHub上进行了修改并提交");
     }
 }
